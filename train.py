3328a0c7151ef34a03a44f12ae737c0cbcbe768d
mdnfiras <mdnfiras@yahoo.com>
2020-11-12 22:25:11 +0100

    fixed training for cml

--- train.py
+++ train.py
@@ -1,9 +1,16 @@
+import os
+import sys
+import urllib.request
 import pandas
 import tensorflow
 import keras
 
 print("Running...")
 
-!if [[ ! -d report ]]; then mkdir report ; fi
-!echo GG > report/metrics.txt
-!wget https://thumbs.dreamstime.com/b/card-minimal-cartoonish-style-vector-templates-nice-job-lettering-speech-bubble-89326194.jpg -P report
+if (not(os.path.isfile("report"))):
+    os.mkdir("report")
+with open("report/metrics.txt", "w") as f:
+    print("Nice job", file=f)
+url = "https://thumbs.dreamstime.com/b/card-minimal-cartoonish-style-vector-templates-nice-job-lettering-speech-bubble-89326194.jpg"
+urllib.request.urlretrieve(url, 'report/image.jpg')
+
